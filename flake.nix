f64995a808bad99d33e412e06cd67ad60b2a28d8
martin.bourbier <martin.bourbier@epita.fr>
2023-11-22 14:52:13 +0100

    feat: basic nix config

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  description = "NixOS configuration";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+    home-manager = {
+      url = "git+https://github.com/nix-community/home-manager?ref=release-23.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-utils.url = "git+https://github.com/numtide/flake-utils?ref=main";
+  };
+
+  outputs = { self, nixpkgs, home-manager, flake-utils }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
+      lib = nixpkgs.lib;
+    in
+    {
+      nixosConfigurations = {
+        martin = lib.nixosSystem {
+          inherit system;
+          modules = [
+            ./configuration.nix
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.martin = {
+                imports = [ ./home.nix ];
+              };
+            }
+          ];
+        };
+      };
+    };
+}
---
+++ home.nix
@@ -0,0 +1,85 @@
+{ config, pkgs, ... }:
+
+{
+  home.username = "martin";
+  home.homeDirectory = "/home/martin";
+  home.stateVersion = "23.05";
+
+  programs.home-manager.enable = true;
+
+  home.packages = with pkgs; [
+    zsh
+    zsh-powerlevel10k
+    git
+    htop
+    meslo-lgs-nf
+    xsel
+    nixpkgs-fmt
+  ];
+
+  programs.zsh = {
+    enable = true;
+    enableAutosuggestions = true;
+    dotDir = ".config/zsh";
+
+    initExtraBeforeCompInit = ''
+      # p10k instant prompt
+      local P10K_INSTANT_PROMPT="$XDG_CACHE_HOME/p10k-instant-prompt-''${(%):-%n}.zsh"
+      [[ ! -r "$P10K_INSTANT_PROMPT" ]] || source "$P10K_INSTANT_PROMPT"
+    '';
+
+    plugins = with pkgs; [
+      {
+        file = "powerlevel10k.zsh-theme";
+        name = "powerlevel10k";
+        src = "${zsh-powerlevel10k}/share/zsh-powerlevel10k";
+      }
+      {
+        file = "p10k.zsh";
+        name = "powerlevel10k-config";
+        src = ./p10k.zsh; # Some directory containing your p10k.zsh file
+      }
+    ];
+  };
+
+  programs.alacritty = {
+    enable = true;
+    settings = {
+      window = {
+        padding = {
+          x = 10;
+          y = 10;
+        };
+        decorations = "full";
+        opacity = 1.0;
+        startup_mode = "Windowed";
+        title = "Alacritty";
+        dynamic_title = true;
+        class = {
+          instance = "Alacritty";
+          general = "Alacritty";
+        };
+      };
+      font = {
+        normal = {
+          family = "MesloLGS NF";
+          style = "regular";
+        };
+        glyph_offset = {
+          x = 0;
+          y = 0;
+        };
+      };
+    };
+  };
+
+  programs.git = {
+    enable = true;
+    userName = "martin.bourbier";
+    userEmail = "martin.bourbier@epita.fr";
+    signing = {
+      key = "555B81DD70BC278778104D82050994FE5A4B2D54";
+      signByDefault = true;
+    };
+  };
+}
