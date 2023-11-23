4cd9545bf2fbd75344802b55984b75081304826a
martin.bourbier <martin.bourbier@epita.fr>
2023-11-23 12:30:29 +0100

    feat: override idea to use the last available tarball

--- home.nix
+++ home.nix
@@ -1,5 +1,4 @@
 { config, pkgs, ... }:
-
 {
   home.username = "martin";
   home.homeDirectory = "/home/martin";
@@ -16,6 +15,14 @@
     xsel
     nixpkgs-fmt
     discord
+    (jetbrains.idea-ultimate.overrideAttrs (oldAttrs: {
+      src = fetchTarball {
+        url = "https://download.jetbrains.com/idea/ideaIU-2023.2.5.tar.gz";
+        sha256 = "13lj46vpvz342lncx147zfyh4ijgk86cwgvd51925kv6mr8bgnqh";
+      };
+
+      version = "2023.2.5";
+    }))
   ];
 
   programs.zsh = {
