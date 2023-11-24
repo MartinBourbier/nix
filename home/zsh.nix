2272f3af7239d2dfb0d02f98caa6b6ab24ca12d9
martin.bourbier <martin.bourbier@epita.fr>
2023-11-24 13:20:18 +0100

    fix(zsh): enforce emacs key mappings

--- home/zsh.nix
+++ home/zsh.nix
@@ -4,11 +4,18 @@
     enable = true;
     enableAutosuggestions = true;
     dotDir = ".config/zsh";
+    defaultKeymap = "emacs";
+    autocd = true;
 
     initExtraBeforeCompInit = ''
       # p10k instant prompt
       local P10K_INSTANT_PROMPT="$XDG_CACHE_HOME/p10k-instant-prompt-''${(%):-%n}.zsh"
       [[ ! -r ${./p10k.zsh} ]] || source ${./p10k.zsh}
+    '';
+
+    initExtra = ''
+      bindkey ";5C" forward-word
+      bindkey ";5D" backward-word
     '';
 
     plugins = with pkgs; [
