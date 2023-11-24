e44800b8375a259890cf801d70d35740be187da7
martin.bourbier <martin.bourbier@epita.fr>
2023-11-24 09:20:29 +0100

    feat(neovim): several upgrades including lsp

--- home.nix
+++ home.nix
@@ -21,12 +21,30 @@
   programs.neovim = {
     enable = true;
     defaultEditor = true;
+    vimAlias = true;
     plugins = with pkgs.vimPlugins; [
       vim-airline
       nvim-lspconfig
       gruvbox
+      coc-json
+      coc-clangd
+      coc-pyright
+      coc-yaml
+      fzf-vim
+      vim-polyglot
     ];
     withNodeJs = true;
+    coc = {
+      enable = true;
+      settings = {
+        languageserver = {
+          nix = {
+            command = "rnix-lsp";
+            filetypes = [ "nix" ];
+          };
+        };
+      };
+    };
     extraConfig = ''
       colorscheme gruvbox
 
@@ -39,7 +57,24 @@
       set autoindent expandtab tabstop=4 softtabstop=4 shiftwidth=4
       set backspace=start,eol,indent
       set hlsearch incsearch
+
+      filetype plugin indent on
+
+      function! s:check_back_space() abort
+        let col = col('.') - 1
+        return !col || getline('.')[col - 1]  =~# '\s'
+      endfunction
+
+      inoremap <silent><expr> <TAB>
+            \ pumvisible() ? "\<C-n>" :
+            \ <SID>check_back_space() ? "\<TAB>" :
+            \ coc#refresh()
+      inoremap <expr><S-TAB> pumvisible() ? "\<C-p>" : "\<C-h>"
     '';
+    extraPackages = with pkgs; [
+      nixfmt
+      rnix-lsp
+    ];
   };
 
   programs.zsh = {
