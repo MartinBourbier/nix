a6917aab12221da006196ccb52ed3f345ca3b0fc
martin.bourbier <martin.bourbier@epita.fr>
2023-11-24 17:46:06 +0100

    fix(configuration): disable mtr

--- configuration.nix
+++ configuration.nix
@@ -107,7 +107,7 @@
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
-  programs.mtr.enable = true;
+  # programs.mtr.enable = true;
 
   # List services that you want to enable:
 
