64694458bca1366860eded6ee8512edeef273965
martin.bourbier <martin.bourbier@epita.fr>
2023-12-13 15:04:33 +0100

    feat: multi git config

--- home/git.nix
+++ home/git.nix
@@ -4,10 +4,21 @@
     enable = true;
     userName = "martin.bourbier";
     userEmail = "martin.bourbier@epita.fr";
-    signing = {
-      key = "555B81DD70BC278778104D82050994FE5A4B2D54";
-      signByDefault = true;
-    };
     delta.enable = true;
+
+    includes = [
+      {
+        condition = "gitdir:/home/martin/workspace/forge/";
+        contents = {
+          user = {
+            name = "Martin Bourbier";
+            email = "martin@forge.epita.fr";
+            signingKey = "555B81DD70BC278778104D82050994FE5A4B2D54";
+          };
+          commit.gpgsign = true;
+          delta.enable = true;
+        };
+      }
+    ];
   };
 }
