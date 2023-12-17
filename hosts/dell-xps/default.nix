b2f23c6d26a7b072ca487ceb86eaeecad7490fc0
martin.bourbier <martin.bourbier@epita.fr>
2023-12-17 10:00:24 +0100

    profiles: add dell-xps

---
+++ hosts/dell-xps/default.nix
@@ -0,0 +1,57 @@
+{ pkgs, lib, config, nixos-hardware, ... }:
+
+{
+  imports = [
+    ../../profiles/core.nix
+    ../../profiles/docker.nix
+    ../../profiles/laptop.nix
+    ../../profiles/yubikey.nix
+    nixos-hardware.nixosModules.common-cpu-intel
+    nixos-hardware.nixosModules.common-pc
+    nixos-hardware.nixosModules.common-pc-ssd
+    nixos-hardware.nixosModules.dell-xps-13-9310
+  ];
+  networking = {
+    hostName = "dell-xps";
+  };
+
+  boot.initrd.availableKernelModules = [ "xhci_pci" "thunderbolt" "nvme" "usb_storage" "sd_mod" ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ "kvm-intel" ];
+  boot.extraModulePackages = [ ];
+
+  fileSystems."/" = {
+    device = "/dev/disk/by-uuid/2d0aa30c-7765-46a3-b3e2-d46cfe0a3a97";
+    fsType = "ext4";
+  };
+
+  boot.initrd.luks.devices = {
+    "luks-a7fca84c-d266-4a04-ab45-646989ed8610".device = "/dev/disk/by-uuid/a7fca84c-d266-4a04-ab45-646989ed8610";
+    # swap
+    "luks-d744a323-7961-43f1-8a8f-5a76c47ce80d".device = "/dev/disk/by-uuid/d744a323-7961-43f1-8a8f-5a76c47ce80d";
+  };
+
+
+  fileSystems."/boot" =
+    {
+      device = "/dev/disk/by-uuid/1E72-6AAF";
+      fsType = "vfat";
+    };
+
+  swapDevices =
+    [{ device = "/dev/disk/by-uuid/5878b4c1-8fc1-4f84-80a1-5860dcc98dbb"; }];
+
+  networking.useDHCP = lib.mkDefault true;
+
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
+  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+
+  # webcam
+  hardware.ipu6 = {
+    enable = true;
+    platform = "ipu6ep";
+  };
+
+  system.stateVersion = "23.11";
+}
