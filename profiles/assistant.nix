7123c0cc66153700f753090852c319be308657c8
martin.bourbier <martin.bourbier@epita.fr>
2024-01-24 10:27:01 +0100

    feat(profiles/assistant): add autotools, valgrind and ctags

--- profiles/assistant.nix
+++ profiles/assistant.nix
@@ -2,6 +2,17 @@
 
 {
   environment.systemPackages = with pkgs; [
+    autoconf
+    autoconf-archive
+    automake
+    autoreconfHook
+    clang-tools
+    gcc
+    gdb
+    gnumake
+    libtool
     slack
+    universal-ctags
+    valgrind
   ];
 }
